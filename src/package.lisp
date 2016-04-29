0903b1726713576f71a7d85819724dae690bfdec
y2q-actionman <y2q-actionman@users.noreply.github.com>
2016-04-29 23:13:01 +0900

    updated defpackage

--- src/package.lisp
+++ src/package.lisp
@@ -2,4 +2,16 @@
 
 (defpackage :scsu
   (:use :cl)
-  )
+  (:export
+   ;; scsu-state object
+   #:*scsu-state-default-fix-dynamic-window*
+   #:scsu-state
+   #:scsu-state-reset
+   ;; errors
+   #:scsu-error
+   ;; main encoder
+   #:decode-to-string
+   #:decode-unit-from-bytes
+   #:encode-from-string
+   #:encode-unit-to-bytes
+   ))
